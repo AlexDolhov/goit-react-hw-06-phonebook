5d9fdf2a7d84836a9d6f2acee7f9289e74f8ef9f
Alex Dolhov <alexdolhov@gmail.com>
2023-01-29 11:55:04 +0200

    init redux, adds store.js, actions.js, selectors.js
    - init redux, react-redux reduxjs/toolkit, redux-devtools/extension
    - adds store.js
    -adds  actions.js, and al needed actions
    -adds  selectors.js and allneeded selectors functions
    -adds addContact action send to contactForm component

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,11 +1,11 @@
-// import { Component } from 'react';
-import PropTypes from 'prop-types';
+import { Component } from 'react';
+import { useDispatch } from 'react-redux';
+import { addContact } from '../../redux/actions';
 import { Label, Input, Button, ErrorText } from './ContactForm.styled';
 import { Formik, Form } from 'formik';
 import * as yup from 'yup';
 
 const nameRegExp = /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/;
-
 const numberRegExp =
   /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/;
 
@@ -28,11 +28,16 @@ let schema = yup.object().shape({
 
 const initialValues = { name: '', number: '' };
 
-export const ContactForm = ({ onSubmit }) => {
+export const ContactForm = () => {
+  const dispatch = useDispatch();
+
   const handleSubmit = (values, { resetForm }) => {
     console.log(values);
     // console.log(actions);
-    onSubmit(values);
+    // onSubmit(values);
+    const { name, number } = values;
+    dispatch(addContact(name, number));
+    // console.log(values);
     resetForm();
   };
 
@@ -57,10 +62,6 @@ export const ContactForm = ({ onSubmit }) => {
       </Form>
     </Formik>
   );
-};
-
-ContactForm.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
 };
 
 // // // // Controlled Form // // // // //
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,28 +1,37 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { List, Item, Button } from './ContactList.styled';
+import { useSelector } from 'react-redux';
+import { getContacts, getFilter } from 'redux/selectors';
 
-const ContactList = ({ contacts, onDeleteContact }) => (
-  <List>
-    {contacts.map(({ id, name, number }) => (
-      <Item key={id}>
-        {name}: {number}
-        <Button type="button" onClick={() => onDeleteContact(id)}>
-          Delete
-        </Button>
-      </Item>
-    ))}
-  </List>
-);
+// const ContactList = ({ contacts, onDeleteContact }) => (
+const ContactList = ({ onDeleteContact }) => {
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+  // const visibleContacts =
 
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ),
-  onDeleteContact: PropTypes.func.isRequired,
+  return (
+    <List>
+      {contacts.map(({ id, name, number }) => (
+        <Item key={id}>
+          {name}: {number}
+          <Button type="button" onClick={() => onDeleteContact(id)}>
+            Delete
+          </Button>
+        </Item>
+      ))}
+    </List>
+  );
 };
+
+// ContactList.propTypes = {
+//   contacts: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.string.isRequired,
+//       name: PropTypes.string.isRequired,
+//       number: PropTypes.string.isRequired,
+//     })
+//   ),
+//   onDeleteContact: PropTypes.func.isRequired,
+// };
 
 export default ContactList;
