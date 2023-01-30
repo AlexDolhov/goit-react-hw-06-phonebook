ca61a526a82077a5b44436dbf91032ea92582808
Alex Dolhov <alexdolhov@gmail.com>
2023-01-30 11:50:40 +0200

    addsrefactoring (store, reducer, actions) to redux/toolkit

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,6 +1,4 @@
-// import { createStore } from 'redux';
-// import { devToolsEnhancer } from '@redux-devtools/extension';
-import { combineReducers } from 'redux';
+import { addContact, deleteContact, setFilter } from './actions';
 
 const contactsInitialState = [
   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
@@ -9,27 +7,57 @@ const contactsInitialState = [
   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 ];
 
-const contactsReducer = (state = contactsInitialState, action) => {
+export const contactsReducer = (state = contactsInitialState, action) => {
   switch (action.type) {
-    case 'contacts/addContact':
+    case addContact.type:
       return [action.payload, ...state];
-    case 'contacts/deleteContact':
+    case deleteContact.type:
       return state.filter(contact => contact.id !== action.payload);
     default:
       return state;
   }
 };
 
-const filterReducer = (state = '', action) => {
+export const filterReducer = (state = '', action) => {
   switch (action.type) {
-    case 'filter/setFilter':
+    case setFilter.type:
       return (state = action.payload);
     default:
       return state;
   }
 };
 
-export const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filter: filterReducer,
-});
+// Before Redux Toolkit
+// import { combineReducers } from 'redux';
+
+// const contactsInitialState = [
+//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+// ];
+
+// const contactsReducer = (state = contactsInitialState, action) => {
+//   switch (action.type) {
+//     case 'contacts/addContact':
+//       return [action.payload, ...state];
+//     case 'contacts/deleteContact':
+//       return state.filter(contact => contact.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+
+// const filterReducer = (state = '', action) => {
+//   switch (action.type) {
+//     case 'filter/setFilter':
+//       return (state = action.payload);
+//     default:
+//       return state;
+//   }
+// };
+
+// export const rootReducer = combineReducers({
+//   contacts: contactsReducer,
+//   filter: filterReducer,
+// });
