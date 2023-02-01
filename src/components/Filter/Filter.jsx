d9ca20096252ea5ae0b97bfbca815a7aaedcb5d3
Alex Dolhov <alexdolhov@gmail.com>
2023-02-01 22:22:13 +0200

    makes all inputs as controlled

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,16 +1,18 @@
 import { Label, Input } from './Filter.styled';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setFilter } from 'redux/filterSlice';
+import { getFilter } from 'redux/selectors';
 
 const Filter = () => {
   const dispatch = useDispatch();
+  const filter = useSelector(getFilter);
   const handleChange = e => {
     dispatch(setFilter(e.currentTarget.value));
   };
   return (
     <Label>
       Find contacts by name
-      <Input type="text" onChange={handleChange} />
+      <Input type="text" value={filter} onChange={handleChange} />
     </Label>
   );
 };
